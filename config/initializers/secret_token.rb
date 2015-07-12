358f5c928705e7f03f92cf1e0f6bcaa5a245b240
Vergetone Productions <mike@vergetone.com>
2015-07-12 12:34:25 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bow::Application.config.secret_key_base = 'c521a79f202de76170e08ffc520f4b947d8c386b1a7a746d126464280ab1a549dc76c6ed96041a0e6dde1994a16ef800f8ae7f9f8d749b09913781989670e86b'
