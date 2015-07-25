f9c1a30e4e188dd0f3e1de0872dff2f06d8f4e16
Vergetone Productions <mike@vergetone.com>
2015-07-24 23:11:39 -0400

    Add digital retail logos to buy section

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+//Album Hover//
+	$('#beYourBow').mouseover(function () {
+		$('#beYourBowInner').fadeIn();
+	});
+	$('#beYourBow').mouseleave(function () {
+		$('#beYourBowInner').fadeOut();
+	});
+	$('#lovesound').mouseover(function () {
+		$('#lovesoundInner').fadeIn();
+	});
+	$('#lovesound').mouseleave(function () {
+		$('#lovesoundInner').fadeOut();
+	});
+//Album Hover//
+});
