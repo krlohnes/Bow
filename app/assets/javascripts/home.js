261217da3d74d1ee393531ea154e0d25215089df
Vergetone Productions <mike@vergetone.com>
2015-07-26 23:14:50 -0400

    Add audio players

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,4 +13,25 @@ $(document).ready(function(){
 		$('#lovesoundInner').fadeOut();
 	});
 //Album Hover//
+//Audio Controls//
+	$('#trackOnePlay').click(function() {
+		if ($('#trackTwo')[0].paused == false) {
+			$('#trackTwo')[0].pause();
+		}
+		if ($('#trackOne')[0].paused == false) {
+			$('#trackOne')[0].pause();
+		} else {
+			$('#trackOne')[0].play();
+		}
+	});
+	$('#trackTwoPlay').click(function() {
+		if ($('#trackOne')[0].paused == false) {
+			$('#trackOne')[0].pause();
+		}
+		if ($('#trackTwo')[0].paused == false) {
+			$('#trackTwo')[0].pause();
+		} else {
+			$('#trackTwo')[0].play();
+		}
+	});
 });
