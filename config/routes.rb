7bb934d15aab9dc839d740bf5870f74bd9f9c4ad
Vergetone Productions <mike@vergetone.com>
2015-09-26 22:34:39 -0400

    Add new page for gold coins game

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Bow::Application.routes.draw do
   get "pages/home"
   root "pages#home"
 
+  get 'goldcoins' => 'pages#goldcoins'
+
   get "test" => "pages#test"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
